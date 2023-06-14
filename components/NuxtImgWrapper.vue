d3a12d9851b5d333a3e8ffced1989c25f963d5c4
jaybharadia <jay.p.bharadia@gmail.com>
2023-06-14 15:18:55 +0530

    Updated version

--- components/NuxtImgWrapper.vue
+++ components/NuxtImgWrapper.vue
@@ -2,6 +2,7 @@
     <NuxtImg
         class="rounded-full"
         preset="avatar"
+        width="50"
         :src="`https://products.7span.in/assets/8d451289-68f6-40ac-9c2a-c2d9b5639960`"
     />
 </template>
