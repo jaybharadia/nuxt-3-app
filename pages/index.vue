79e046066217dc8075f00713b5e415a168d7433a
jaybharadia <jay.p.bharadia@gmail.com>
2023-09-06 22:18:14 +0530

    Checking done

--- pages/index.vue
+++ pages/index.vue
@@ -8,29 +8,39 @@
 
 <script setup>
 const { data } = await useAsyncData("features", async () => {
-    // const user = await new Promise((resolve) => {
-    //     setTimeout(() => {
-    //         resolve({
-    //             name: "jay",
-    //             age: 20,
-    //         });
-    //     }, 3000);
-    // });
+    return new Promise(async (resolve) => {
+        const user = await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({
+                    name: "jay",
+                    age: 20,
+                });
+            }, 3000);
+        });
 
-    // const item = await new Promise((resolve) => {
-    //     setTimeout(() => {
-    //         resolve({
-    //             item: "asdasidjasdij2993293",
-    //         });
-    //     }, 1000);
-    // });
+        const item = await new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({
+                    item: "asdasidjasdij2993293",
+                });
+            }, 1000);
+        });
+        resolve({
+            user,
+            item,
+        });
+    });
 
     // const item = await useUser();
 
-    const item = await useFeaturedArticle();
+    // const item = await useFeaturedArticle();
 
-    return item;
+    // return item;
 });
+console.log(
+    "🚀 ~ file: index.vue:40 ~ const{data}=awaituseAsyncData ~ data:",
+    data
+);
 definePageMeta({
     layout: "mobile",
 });
