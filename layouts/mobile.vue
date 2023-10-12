b9cabae881fe598c8550ca9348bdf19434272a75
jaybharadia <jay.p.bharadia@gmail.com>
2023-10-12 21:14:23 +0530

    clean up files

--- layouts/mobile.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <Title>Mobile Layout</Title>
-    <div>Inside Mobile Layout</div>
-
-    <slot />
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,5 @@
 <template>
-    <div>
-        Version {{ version }}
-        <div>Inside Index PAGE</div>
-
-        <!-- <Counter /> -->
-
-        <NuxtImgWrapper />
-    </div>
+    <div></div>
 </template>
 
-<script setup>
-import { version } from "../package.json";
-
-const { data } = await useAsyncData("features", async () => {
-    // const user = await new Promise((resolve) => {
-    //     setTimeout(() => {
-    //         resolve({
-    //             name: "jay",
-    //             age: 20,
-    //         });
-    //     }, 3000);
-    // });
-
-    // const item = await new Promise((resolve) => {
-    //     setTimeout(() => {
-    //         resolve({
-    //             item: "asdasidjasdij2993293",
-    //         });
-    //     }, 1000);
-    // });
-
-    const item = await useUser();
-
-    return item;
-});
-definePageMeta({
-    layout: "mobile",
-});
-</script>
+<script setup></script>
